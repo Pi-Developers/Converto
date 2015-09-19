22cf10a28560941668ff3f1110c93f565ba85f42
Sahid S Almas <robinaf3@gmail.com>
2015-09-19 16:21:12 +0530

    Update Converto.java

--- Java/Converto.java
+++ Java/Converto.java
@@ -1,10 +1,23 @@
+/*
+ * Copyright (C) 2015 Pi Developers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Sahid Almas
+ */
 import java.util.Scanner;
 
 
-/**
- * @author Sahid S Almas
- */
- 
 public class Converto {
     private static Scanner mScanner;
     private static int SELECTED_ON_ENG_TO_ARA = 1;
