ed962738f8444c732692c4dbe23a526ad02b1d65
Sahid S Almas <robinaf3@gmail.com>
2015-09-19 16:20:53 +0530

    added docs

--- Java/ConvertoGUI.java
+++ Java/ConvertoGUI.java
@@ -1,11 +1,25 @@
+/*
+ * Copyright (C) 2015 Pi Developers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Sahid Almas
+ */
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
- * @author Sahid S Almas
- */
 public class ConvertoGUI {
     public static  String INPUT = null;
     public static void main(String[] args) {
